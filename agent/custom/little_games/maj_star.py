7e7eb374744c01d124b1af07a088dd30c539a668
azmiao <2362020227@qq.com>
2026-03-29 12:00:53 +0800

    feat✨: 新增单人匹配模式的麻将挂机功能

--- agent/custom/little_games/maj_star.py
+++ agent/custom/little_games/maj_star.py
@@ -6,7 +6,6 @@ from maa.custom_action import CustomAction, RecognitionDetail
 
 from agent.attach.common_attach import get_maj_team_type
 from agent.constant.map_point import NAVIGATE_DATA
-from agent.custom.app_manage_action import wait_for_switch
 from agent.custom.general.general import ensure_main_page
 from agent.custom.general.power_saving_mode import exit_power_saving_mode
 from agent.custom.teleport_action import teleport_or_navigate
@@ -56,17 +55,21 @@ class MajStarPointAction(CustomAction):
                 has_entry = ensure_maj_entry(context)
                 if not has_entry:
                     return False
-
                 # 点击雀牌游戏
                 context.tasker.controller.post_click(857, 342).wait()
-                time.sleep(1)
+                time.sleep(2)
 
-            # TODO 确保开始游戏
+            # 确保开始对局
+            has_next = ensure_into_game(context, is_leader)
+            if not has_next:
+                return False
 
-            # 等待场景切换完成
-            wait_for_switch(context)
-
-            # TODO 正式的游戏环节
+            # 正式的游戏环节
+            has_next = maj_task_cycle(context)
+            if not has_next:
+                return False
+            # 5秒后开始下一轮
+            time.sleep(5)
 
         logger.warning("麻将任务已结束！")
         return True
@@ -89,10 +92,107 @@ def ensure_maj_entry(context: Context, timeout: int = 120) -> bool:
     while elapsed_time <= timeout and not context.tasker.stopping:
         elapsed_time = time.time() - start_time
         if check_is_entry(context):
-            logger.info(f"检测到已经到达麻将的入口！")
+            logger.info(f"已经到达麻将的入口，尝试点击开始游戏")
             return True
         time.sleep(2)
     logger.error("超 120 秒未到达麻将的入口！")
+    return False
+
+
+def ensure_into_game(context: Context, is_leader: bool, timeout: int = 300) -> bool:
+    """
+    确保开始对局
+    """
+    # 循环检测是否到准备页面
+    start_time = time.time()
+    elapsed_time = 0
+
+    # 循环等待游戏开始
+    while elapsed_time <= timeout and not context.tasker.stopping:
+        elapsed_time = time.time() - start_time
+        time.sleep(2)
+
+        # 如果在对局中就返回
+        if check_in_match(context):
+            return True
+
+        if check_is_ready(context):
+            logger.info("尝试点击进入麻将对局...")
+            # 点击确认进入副本
+            time.sleep(0.5)
+            context.tasker.controller.post_click(1143, 618).wait()
+
+        if is_leader:
+            # 没进入副本就再次检测
+            if check_is_entry(context):
+                # 点击雀牌游戏
+                context.tasker.controller.post_click(857, 342).wait()
+                time.sleep(2)
+
+    logger.error(f"超 300 秒未开始麻将对局！")
+    return False
+
+
+def maj_task_cycle(context: Context) -> bool:
+    """
+    正式的麻将对局循环
+    """
+    while not context.tasker.stopping:
+        time.sleep(1.5)
+        # 随便点击个位置防止月卡或者锁屏
+        context.tasker.controller.post_click(638, 343).wait()
+        time.sleep(0.5)
+
+        img = context.tasker.controller.post_screencap().wait().get()
+        # 检测自动和牌是否未开启
+        reg_result: RecognitionDetail | None = context.run_recognition("检测自动和牌是否未开启", img)
+        if reg_result and reg_result.hit:
+            context.tasker.controller.post_click(28, 430).wait()
+            time.sleep(0.5)
+        # 检测跳过鸣牌是否未开启
+        reg_result: RecognitionDetail | None = context.run_recognition("检测跳过鸣牌是否未开启", img)
+        if reg_result and reg_result.hit:
+            context.tasker.controller.post_click(28, 484).wait()
+            time.sleep(0.5)
+        # 检测自动摸切是否未开启
+        reg_result: RecognitionDetail | None = context.run_recognition("检测自动摸切是否未开启", img)
+        if reg_result and reg_result.hit:
+            context.tasker.controller.post_click(28, 537).wait()
+            time.sleep(0.5)
+        # 检测是否需要点击跳过按钮 | 暗杠
+        ocr_result: RecognitionDetail | None = context.run_recognition(
+            "通用文字识别",
+            img,
+            pipeline_override={
+                "通用文字识别": {
+                    "expected": "跳过",
+                    "roi": [745, 541, 144, 69],
+                }
+            },
+        )
+        if ocr_result and ocr_result.hit:
+            context.tasker.controller.post_click(815, 576).wait()
+            time.sleep(0.5)
+
+        # 检测是否终局
+        ocr_result: RecognitionDetail | None = context.run_recognition(
+            "通用文字识别",
+            img,
+            pipeline_override={
+                "通用文字识别": {
+                    "expected": "终局",
+                    "roi": [565, 66, 144, 69],
+                }
+            },
+        )
+        if ocr_result and ocr_result.hit:
+            logger.info(f"本局麻将对局已完成！")
+            # 点击确认按钮
+            time.sleep(1.5)
+            context.tasker.controller.post_click(639, 662).wait()
+            time.sleep(0.5)
+            return True
+
     return False
 
 
@@ -106,13 +206,48 @@ def check_is_entry(context: Context) -> bool:
         img,
         pipeline_override={
             "通用文字识别": {
-                "expected": "雀牌     ",
+                "expected": "雀牌",
                 "roi": [874, 330, 44, 29],
             }
         },
     )
     if ocr_result and ocr_result.hit:
-        logger.info(f"检测到已经到达麻将的入口！")
+        logger.info(f"检测到已经到达麻将的入口")
+        return True
+    else:
+        return False
+
+
+def check_is_ready(context: Context) -> bool:
+    """
+    检测是否进入准备页面
+    """
+    img = context.tasker.controller.post_screencap().wait().get()
+    ocr_result: RecognitionDetail | None = context.run_recognition(
+        "通用文字识别",
+        img,
+        pipeline_override={
+            "通用文字识别": {
+                "expected": ["接受"],
+                "roi": [1113, 603, 56, 27]
+            }
+        },
+    )
+    if ocr_result and ocr_result.hit:
+        logger.info(f"检测到准备按钮，准备进入麻将...")
+        return True
+    else:
+        return False
+
+
+def check_in_match(context: Context) -> bool:
+    """
+    检测是否在对局中
+    """
+    img = context.tasker.controller.post_screencap().wait().get()
+    reg_result: RecognitionDetail | None = context.run_recognition("检测是否已经在麻将对局中", img)
+    if reg_result and reg_result.hit:
+        logger.info(f"检测到已经在对局中...")
         return True
     else:
         return False
