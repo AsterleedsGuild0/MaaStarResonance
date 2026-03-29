13c044c269e1d67c45887a8ac36e5425893d2131
AZMIAO <54100327+azmiao@users.noreply.github.com>
2026-03-29 14:10:28 +0800

    feat ✨: 合并提交 - 新增躲猫猫速刷功能 & 麻将挂机功能（支持单人/组队队长/组队队员） (#84)
    * feat✨: 搭建躲猫猫小游戏匹配模式基本架构逻辑
    
    * feat✨: 新增躲猫猫不同队伍模式的基本架构逻辑
    
    * feat✨: 添加躲猫猫小游戏的任务接口
    
    * feat✨: 不思议的追讨游戏_速刷_队员端
    
    * feat✨: 不思议的追逃游戏_速刷_队长端
    
    * patch🚑: 补充完整躲猫猫正常挂机逻辑
    
    * chore🔧: 迁移躲猫猫和麻将合并至一个文件夹
    
    * feat✨: 新增麻将挂机的基本逻辑框架，整理代码结构
    
    * feat✨: 新增单人匹配模式的麻将挂机功能
    
    * fix🐛: 兼容四人组队麻将队长和队员模式
    
    * deps🧩: 更新MFAA至v2.11.6
    
    * docs📚: 更新文档
    
    * fix🐛: 修复计数问题
    
    ---------
    
    Co-authored-by: 233Official <ayusummer233@outlook.com>

--- agent/attach/common_attach.py
+++ agent/attach/common_attach.py
@@ -212,3 +212,39 @@ def get_need_cocoon_name(context: Context) -> str:
                          ) if cocoon_node else ""
     logger.info("需要刷的茧: {}", str(cocoon_name))
     return str(cocoon_name)
+
+
+def get_hide_team_type(context: Context) -> str:
+    """
+    获取躲猫猫队伍类型：
+    1. 无
+    2. 单人匹配游戏
+    3. 组队匹配游戏（队长）
+    4. 组队匹配游戏（队员）
+    5. 组队私人游戏（队长，队伍人数须>=5）
+    6. 组队私人游戏（队员）
+    """
+    hide_team_type_node = context.get_node_data("获取参数-躲猫猫队伍类型")
+    hide_team_type = (hide_team_type_node
+                         .get("attach", {})
+                         .get("hide_team_type", "无")
+                         ) if hide_team_type_node else "无"
+    logger.info("躲猫猫队伍类型: {}", str(hide_team_type))
+    return str(hide_team_type)
+
+
+def get_maj_team_type(context: Context) -> str:
+    """
+    获取麻将队伍类型：
+    1. 无
+    2. 单人匹配游戏
+    3. 组队私人游戏（队长）
+    4. 组队私人游戏（队员）
+    """
+    maj_team_type_node = context.get_node_data("获取参数-麻将队伍类型")
+    maj_team_type = (maj_team_type_node
+                         .get("attach", {})
+                         .get("maj_team_type", "无")
+                         ) if maj_team_type_node else "无"
+    logger.info("麻将队伍类型: {}", str(maj_team_type))
+    return str(maj_team_type)
---
+++ agent/custom/little_games/hide_and_seek.py
@@ -0,0 +1,258 @@
+import time
+
+from maa.agent.agent_server import AgentServer
+from maa.context import Context
+from maa.custom_action import CustomAction, RecognitionDetail
+
+from agent.attach.common_attach import get_hide_team_type
+from agent.constant.map_point import NAVIGATE_DATA
+from agent.custom.app_manage_action import wait_for_switch
+from agent.custom.general.general import ensure_main_page
+from agent.custom.general.power_saving_mode import exit_power_saving_mode
+from agent.custom.teleport_action import teleport_or_navigate
+from agent.logger import logger
+from agent.utils.param_utils import CustomActionParam
+
+
+@AgentServer.custom_action("HideSeekPoint")
+class HideSeekPointAction(CustomAction):
+
+    def __init__(self):
+        super().__init__()
+        self.game_count = None
+
+    @exit_power_saving_mode()
+    @ensure_main_page()
+    def run(
+        self,
+        context: Context,
+        argv: CustomAction.RunArg,
+    ) -> bool:
+
+        self.game_count = 0
+
+        # 获取参数
+        params = CustomActionParam(argv.custom_action_param)
+        max_game_count = int(params.data["max_game_count"]) if params.data["max_game_count"] else 0
+        logger.info(f"本次任务设置的最大躲猫猫次数: {max_game_count if max_game_count != 0 else '无限'}")
+
+        # 躲猫猫队伍类型
+        hide_team_type = get_hide_team_type(context)
+        if hide_team_type == "无":
+            logger.error("请先选择躲猫猫队伍类型！")
+            return False
+        # 是否是队长：队长要去NPC那边开本，队员不用干活
+        is_leader = hide_team_type in ["单人匹配游戏", "组队匹配游戏（队长）", "组队私人游戏（队长，队伍人数须>=5）"]
+        # 是否是开黑模式：开黑模式直接点开始按钮，匹配模式要等待匹配到              
+        is_private = hide_team_type in ["组队私人游戏（队长，队伍人数须>=5）", "组队私人游戏（队员）"]
+
+        while not context.tasker.stopping:
+            logger.info(f"=== 已成功躲猫猫 {self.game_count} 次 ===")
+            # 检查是否已经躲猫猫足够次数了
+            if max_game_count != 0 and max_game_count <= self.game_count:
+                logger.info(f"已成功躲猫猫了您所配置的{self.game_count}次，躲猫猫结束！")
+                return True
+            
+            if is_leader:
+                # 确保到达躲猫猫的入口
+                has_entry = ensure_hide_entry(context)
+                if not has_entry:
+                    return False
+            
+                # 点击不思议的追逃游戏
+                context.tasker.controller.post_click(852, 400).wait()
+                time.sleep(1)
+            
+            # 确保开始游戏 | 匹配/开黑模式
+            has_next = ensure_into_game(context, is_leader, is_private)
+            if not has_next:
+                logger.error("无法开始游戏，躲猫猫任务将直接停止...")
+                return False
+
+            # 等待场景切换完成
+            wait_for_switch(context)
+
+            # 等待躲猫猫游戏对局结束
+            ensure_for_end(context)
+            # 等待场景切换完成后开始下一轮
+            wait_for_switch(context)
+
+            if is_leader:
+                # 多等待10秒 | 对齐不同设备切换场景的加载速度
+                logger.info(f"等待10秒后进行下一轮躲猫猫...")
+                time.sleep(10)
+
+            self.game_count += 1
+        
+        logger.warning("躲猫猫任务已结束！")
+        return True
+
+
+def ensure_into_game(context: Context, is_leader: bool, is_private: bool, timeout: int = 300) -> bool:
+    """
+    确保开始游戏 | 匹配/开黑模式
+    """
+    # 循环检测是否到准备页面
+    start_time = time.time()
+    elapsed_time = 0
+
+    # 循环等待游戏开始
+    while elapsed_time <= timeout and not context.tasker.stopping:
+        logger.info(f"检测并准备进入游戏")
+        elapsed_time = time.time() - start_time
+
+        if not is_leader:
+            if check_is_ready(context):
+                # 点击确认进入副本
+                context.tasker.controller.post_click(1148, 657).wait()
+                return True
+            # 没有就继续循环 | 不执行下面队长的逻辑
+            continue
+
+        img = context.tasker.controller.post_screencap().wait().get()
+        if is_private:
+            # 点击右下角开黑模式
+            ocr_result: RecognitionDetail | None = context.run_recognition(
+                "通用文字识别",
+                img,
+                pipeline_override={
+                    "通用文字识别": {
+                        "expected": "开黑模式",
+                        "roi": [941, 641, 98, 29],
+                    }
+                },
+            )
+            if ocr_result and ocr_result.hit:
+                context.tasker.controller.post_click(990, 656).wait()
+        else:
+            # 点击右下角匹配模式
+            ocr_result: RecognitionDetail | None = context.run_recognition(
+                "通用文字识别",
+                img,
+                pipeline_override={
+                    "通用文字识别": {
+                        "expected": "匹配进入",
+                        "roi": [1128, 641, 93, 31],
+                    }
+                },
+            )
+            if ocr_result and ocr_result.hit:
+                context.tasker.controller.post_click(1175, 656).wait()
+
+        # 每隔两秒检测一次
+        time.sleep(2)
+        if check_is_ready(context):
+            return True
+        
+        time.sleep(2)
+        if check_is_ready(context):
+            return True
+    
+    logger.error(f"超 300 秒未确保开始游戏！")
+    return False
+
+
+def ensure_hide_entry(context: Context, timeout: int = 120) -> bool:
+    """
+    确保到达躲猫猫的入口
+    """
+    # 先检测一下是否可以直接进
+    if check_is_entry(context):
+        return True
+
+    # 不行才导航过去
+    teleport_or_navigate(context, "游星岛", "不思议的追逃游戏", "导航", NAVIGATE_DATA)
+
+    # 循环检测是否到达躲猫猫入口
+    start_time = time.time()
+    elapsed_time = 0
+    while elapsed_time <= timeout and not context.tasker.stopping:
+        elapsed_time = time.time() - start_time
+        if check_is_entry(context):
+            logger.info(f"检测到已经到达躲猫猫的入口！")
+            return True
+        time.sleep(2)
+    logger.error(f"超 {timeout} 秒未到达躲猫猫的入口！")
+    return False
+
+
+def ensure_for_end(context: Context, timeout: int = 1200) -> bool:
+    """
+    确保躲猫猫游戏对局结束
+    """
+    start_time = time.time()
+    elapsed_time = 0
+    while elapsed_time <= timeout and not context.tasker.stopping:
+        elapsed_time = time.time() - start_time
+        if wait_for_end(context):
+            logger.info(f"检测到躲猫猫游戏对局结束！")
+            return True
+        time.sleep(5)
+    logger.error(f"超 {timeout} 秒躲猫猫游戏对局未结束！")
+    return False
+
+
+def wait_for_end(context: Context):
+    """
+    等待游戏彻底结束
+    """
+    img = context.tasker.controller.post_screencap().wait().get()
+    ocr_result: RecognitionDetail | None = context.run_recognition(
+        "通用文字识别",
+        img,
+        pipeline_override={
+            "通用文字识别": {
+                "expected": "退出副本",
+                "roi": [67, 29, 90, 29],
+            }
+        },
+    )
+    if ocr_result and ocr_result.hit:
+        logger.info(f"检测到躲猫猫游戏对局已结束")
+        return True
+    else:
+        return False
+
+
+def check_is_ready(context: Context) -> bool:
+    """
+    检测是否进入准备页面
+    """
+    img = context.tasker.controller.post_screencap().wait().get()
+    ocr_result: RecognitionDetail | None = context.run_recognition(
+        "通用文字识别",
+        img,
+        pipeline_override={
+            "通用文字识别": {
+                "expected": ["(确认|取消)"],
+                "roi": [1128, 640, 58, 35]
+            }
+        },
+    )
+    if ocr_result and ocr_result.hit:
+        logger.info(f"检测到准备按钮，准备进入躲猫猫...")
+        return True
+    else:
+        return False
+
+
+def check_is_entry(context: Context) -> bool:
+    """
+    检测当前是否到躲猫猫的入口处
+    """
+    img = context.tasker.controller.post_screencap().wait().get()
+    ocr_result: RecognitionDetail | None = context.run_recognition(
+        "通用文字识别",
+        img,
+        pipeline_override={
+            "通用文字识别": {
+                "expected": "不思议",
+                "roi": [874, 387, 64, 26],
+            }
+        },
+    )
+    if ocr_result and ocr_result.hit:
+        logger.info(f"检测到已经到达躲猫猫的入口！")
+        return True
+    else:
+        return False
